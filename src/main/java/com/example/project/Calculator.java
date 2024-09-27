c905038d3cbb46616252e771003dcb46d925091d
raffin44 <nashwannafi@gmail.com>
2024-09-27 23:05:12 +0000

    message

--- src/main/java/com/example/project/Calculator.java
+++ src/main/java/com/example/project/Calculator.java
@@ -1,12 +1,13 @@
 package com.example.project;
 public class Calculator{
     public String calcModel;
-    
+   
     //constructor that creates an instance of the calculator class
-    public Calculator() {
+    public Calculator(String name) {
         //implement code here
+       
     }
-    
+   
     // method that performs the operation designated by operand (which
     // could be +, -, *, or /) and returns the result of that operation
     // performed between num1 and num2
@@ -15,38 +16,55 @@ public class Calculator{
     // REMINDER: use .equals to test if one string is equal to another (not ==)
     // (THIS METHOD DOES NOT PRINT)
     public double performOperation(String operand, int num1, int num2) {
-        //implement code here
+        // implement code here
+        if (operand.equals("+")) {
+            return num1 + num2;
+        } else if (operand.equals("-")) {
+            return num1-num2;
+        } else if(operand.equals("*")) {
+            return num1*num2;
+        } else if (operand.equals("/")) {
+            return num1/num2;
+        } else
         return 0.00;
     }
+
 
     // method that checks if num1 is evenly divisible by num2, and if so,
     // returns true, and if not, returns false
     // (THIS METHOD DOES NOT PRINT)
     public boolean divisibleBy(int num1, int num2) {
         //implement code here
-        return false;
+        if (num1 % num2 != 0) {
+            return false;
+        } else
+            return true;
     }
+
 
     // method that constructs and returns a string representing a coordinate
     // pair in the format: "(x, y)"
     // (THIS METHOD DOES NOT PRINT)
     public String coordinatePair(int x, int y) {
         //implement code here
-        return "";
+        return ("(" + x + "," + y + ")");
     }
     // method that determines and returns the result of |num1 - num2|
     // (THIS METHOD DOES NOT PRINT)
     public int absoluteValue(int num1, int num2) {
         //implement code here
-        return 0;
+        if (num1 - num2 < 0){
+            return ((num1-num2)*(-1));
+        } else
+            return (num1-num2);
     }
-    
+   
     // method that constructs and returns a String in the format:
     // "This calculator is a model "+____+" where "+____+" should be filled in
     // with the value stored in the calcModel instance variable."
     // (THIS METHOD DOES NOT PRINT)
     public String info() {
         //implement code here
-        return "";
+        return ("This calculator is a model " + calcModel + " where " + calcModel + " should be filled in with the value stored in the calcModel instance variable.");
     }
 }
--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -1,12 +1,51 @@
 package com.example.project;
+import java.util.Scanner;
+
 
 public class CalculatorRunner {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
         //test your program here
         //1. create an instance of the calculator class
-
+        Calculator Babar = new Calculator("TI84");
         //2. call any methods of that class to test
+        System.out.println("Hello! Welcome to Java Calculations!");
+        System.out.println("Please enter + - * or / to add, subtract, multiply or divide.");
+        String operand = scan.nextLine();
+        System.out.println("Enter your two integers: ");
+        int num1 = scan.nextInt();
+        scan.nextLine();
+        int num2 = scan.nextInt();
+        scan.nextLine();
+        System.out.println("The result of " + num1 + " " + operand + " " + num2 + " is " + Babar.performOperation(operand, num1, num2));
 
+
+        System.out.println("Enter two numbers that you would like to check for their divisibility.");
+        int divisible1 = scan.nextInt();
+        scan.nextLine();
+        int divisible2 = scan.nextInt();
+        scan.nextLine();
+        System.out.println(divisible1 + " is divisible by " + divisible2 + ": " + Babar.divisibleBy(divisible1, divisible2));
+
+
+        System.out.println("Enter an x and a y coordinate.");
+        int x = scan.nextInt();
+        scan.nextLine();
+        int y = scan.nextInt();
+        scan.nextLine();
+        System.out.println(Babar.coordinatePair(x, y));
+
+
+        System.out.println("Enter two numbers you would like to subtract, and we will return the absolute value of the subtraction.");
+        int absValue1 = scan.nextInt();
+        scan.nextLine();
+        int absValue2 = scan.nextInt();
+        scan.nextLine();
+        System.out.println("The absolute value of " + absValue1 + " - " + absValue2 + " is " + Babar.absoluteValue(absValue1, absValue2));
+
+
+        System.out.println(Babar.info());
+       
+        scan.close();
     }
 }
